2752fb213d0da9e81b4db57121db657f6bc5343d
sunkariindu <161712733+sunkariindu@users.noreply.github.com>
2024-03-01 19:27:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function (req, res, next) {
 
 //MongoDB Atlas connection using native driver
 // const MongoClient = require('mongodb').MongoClient;
-const uri ='mongodb+srv://indus:nBWd2YtYgac2tnNC@cluster0.4ou0h8h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri ='mongodb+srv://indus:nBWd2YtYgac2tnNC@cluster0.pszusn4.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {});
 

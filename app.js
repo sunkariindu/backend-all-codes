ba93495bcf07b49e64eb12bd9f170211f0f01911
sunkariindu <161712733+sunkariindu@users.noreply.github.com>
2024-03-01 18:58:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,70 +13,57 @@ const shipmentRoute = require('./routes/shipmentRoute');
 const paymentRoute = require('./routes/paymentRoute');
 const appRoute = require('./routes/route');
 const appController = require('./mailer/controller/appController');
-const pincodeRoute = require('./routes/pincodeRoute.js');
-// const uploadModel = require('./models/uploadModel');
- 
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const path = require('path');
- 
+const { MongoClient } = require('mongodb');
+const pincodeRoute = require('./routes/pincodeRoute.js');
 const port = process.env.PORT || 8080;
  
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin',"*");
-//   res.header('Access-Control-Allow-Headers', true);
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
-
-// // //Connect to MongoDB
-// // mongoose.connect('mongodb://localhost:27017/datamodaling', {  });
-// // const db = mongoose.connection;
- 
-// // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// // db.once('open', () => {
-// //     console.log('Connected to MongoDB');
-// // });
-
-// //MongoDB Atlas connection using native driver
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = 'mongodb+srv://indus:QJj4xBy0G8N5IQCM@cluster0.hdmxbb1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-// const client = new MongoClient(uri, {});
-
-// //Connect to MongoDB Atlas
-// client.connect(err => {
-//     if (err) {
-//         console.error('Error connecting to MongoDB Atlas:', err);
-//     } else {
-//         console.log('Connected to MongoDB Atlas');
-//     }
-// });
-
-// //Mongoose setup
-// mongoose.connect(uri, { });
+// //Connect to MongoDB
+// mongoose.connect('mongodb://localhost:27017/datamodaling', {  });
 // const db = mongoose.connection;
-
-// db.on('error', (error) => {
-//     console.error('Error connecting to MongoDB Atlas with Mongoose:', error);
-// });
-
+ 
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // db.once('open', () => {
-//     console.log('Connected to MongoDB Atlas with Mongoose');
+//     console.log('Connected to MongoDB');
 // });
 
- mongoose.connect('mongodb+srv://indus:QJj4xBy0G8N5IQCM@cluster0.hdmxbb1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  
+//MongoDB Atlas connection using native driver
+// const MongoClient = require('mongodb').MongoClient;
+const uri ='mongodb+srv://indusunkari7:FR5ajziZ3Qu4vAa6@cluster0.pszusn4.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {});
+
+//Connect to MongoDB Atlas
+client.connect(err => {
+    if (err) {
+        console.error('Error connecting to MongoDB Atlas:', err);
+    } else {
+        console.log('Connected to MongoDB Atlas');
+    }
 });
+
+//Mongoose setup
+mongoose.connect(uri, { });
 const db = mongoose.connection;
+
 db.on('error', (error) => {
-  console.error('Error connecting to MongoDB Atlas with Mongoose:', error);
+    console.error('Error connecting to MongoDB Atlas with Mongoose:', error);
 });
+
 db.once('open', () => {
-  console.log('Connected to MongoDB Atlas with Mongoose');
+    console.log('Connected to MongoDB Atlas with Mongoose');
 });
+
 app.use(express.json());
 app.use(cors());
  
@@ -85,16 +72,15 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
  
  
-var client = new twilio(accountSid,authToken)
+// var client = new twilio(accountSid,authToken);
 var otp =Math.floor(100000+Math.random()*90000);
 var userEnteredOTP=''
 console.log(otp)
 app.post('/sendotp',(req,res)=>{
     client.messages.create({
-      body:`Hello Nikhila, your OTP is: ${otp}`,
-              to:'+919963760431',
-              from:'+13202454091'
-       
+        body:`hello hi nikhila ${otp}`,
+        to:'+919963760431',
+        from:'+19203546973'
     })
     .then((message)=>{
         console.log('message sent:', message.sid);
@@ -116,19 +102,21 @@ app.post('/verifyotp', (req, res) => {
     }
 });
  
+ 
 app.use('/api', authRoute);
 app.use('/api', router);
 app.use('/api', shipmentRoute);
 app.use('/api', paymentRoute);
 app.use('/api', appRoute);
 app.use('/api',pincodeRoute);
+
 app.use(cors());
+
 app.use(bodyParser.json());
 app.use(fileUpload({ createParentPath: true }));
+
  
- 
- 
- 
+// Handle JSON payload
 app.post('/json-payload', (req, res) => {
   try {
     const data = req.body;
@@ -139,37 +127,24 @@ app.post('/json-payload', (req, res) => {
     res.status(500).json({ status: 'error', message: 'Internal server error' });
   }
 });
- 
+
+// Handle file upload
 app.post('/upload', (req, res) => {
   try {
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).json({ status: 'error', message: 'No files were uploaded.' });
     }
  
-    let files = req.files.file;
+    const file = req.files.file;
+    const uploadPath = path.join(__dirname, 'uploads', file.name);
  
-    if (!Array.isArray(files)) {
-      files = [files];
-    }
- 
-    files.forEach((file) => {
-      if (file) {
-        const uploadPath = path.join(__dirname, 'uploads', file.name);
- 
-        file.mv(uploadPath, (err) => {
-          if (err) {
-            console.error('Error handling file upload:', err);
-            return res.status(500).json({ status: 'error', message: 'Internal server error' });
-          }
- 
-         
-          console.log('File uploaded successfully:', file.name);
-        });
+    file.mv(uploadPath, (err) => {
+      if (err) {
+        return res.status(500).json({ status: 'error', message: err.message });
       }
-    });
  
-   
-    res.json({ status: 'success', message: 'Files uploaded successfully' });
+      res.json({ status: 'success', message: 'File uploaded successfully' });
+    });
   } catch (error) {
     console.error('Error handling file upload:', error);
     res.status(500).json({ status: 'error', message: 'Internal server error' });
@@ -180,6 +155,8 @@ app.get('/', (req, res) => {
     res.send('welcome to snv');
 });
  
+ 
+// Define User Schema
 const userSchema = new Schema({
   name: { type: String, unique: true },
   email: { type: String, unique: true },
@@ -189,12 +166,11 @@ const userSchema = new Schema({
   cruds: [{ type: Schema.Types.ObjectId, ref: 'Crud' }],
   shipments: [{ type: Schema.Types.ObjectId, ref: 'Shipment' }],
   paymentcontrollers: [{ type: Schema.Types.ObjectId, ref: 'PaymentController' }],
-  uploads: [{ type: Schema.Types.ObjectId, ref: 'Upload' }],
 });
  
 const User = mongoose.model('User', userSchema);
  
- 
+// Define Crud Schema
 const crudSchema = new Schema({
   order: { type: String, unique: true },
   date: { type: String, unique: true },
@@ -209,7 +185,7 @@ const crudSchema = new Schema({
  
 const Crud = mongoose.model('Crud', crudSchema);
  
- 
+// Define Shipment Schema
 const shipmentSchema = new Schema({
   order_Id: { type: Number, unique: true },
   customer_Name: { type: String, unique: true },
@@ -224,7 +200,7 @@ const shipmentSchema = new Schema({
  
 const Shipment = mongoose.model('Shipment', shipmentSchema);
  
- 
+// Define PaymentController Schema
 const paymentControllerSchema = new Schema({
   name: { type: String, unique: true },
   amount: { type: Number, unique: true },
@@ -237,43 +213,42 @@ const paymentControllerSchema = new Schema({
  
 const PaymentController = mongoose.model('PaymentController', paymentControllerSchema);
  
- // Create a new user, crud, shipment, upload, and paymentController associated with each other
+// Create a new user, crud, shipment, and paymentController associated with each other
 app.get('/created', async (req, res) => {
   try {
     const newUser = await User.create({
-      name: 'vani',
-      email: 'vrani@example.com',
-      mobile: 99665422134,
-      password: 'bdyt',
-      companyname: 'jghg',
-      
+      name: 'JohnDoe',
+      email: 'john@example.com',
+      mobile: 9898274754,
+      password: 'Indu',
+      companyname: 'snvsolutions',
     });
-    
+ 
     const newCrud = await Crud.create({
-      order: 'nhjghg',
-      date: '9/05/2353',
-      payment: '57654',
-      product: 'vdfre',
-      customer: 'rerew',
-      phone: 7655431217,
-      weight:5.9,
-
+      order: 'pizza',
+      date: '23/12/2023',
+      payment: 50000,
+      product: 'fooditems',
+      customer: 'manga',
+      phone: 986878699,
+      weight: '2.5',
+      user: newUser._id,
     });
-    
+ 
     const newShipment = await Shipment.create({
-      order_Id: 4543545,
-      customer_Name: 'rao',
-      customer_Address: 'wazeed',
-      billing_Num: '44232',
-      pickup_loc: 'kaliveru',
-      pin_Code: 598234,
-      shipping_Date: '6/3/2053',
+      order_Id: 565565,
+      customer_Name: 'mangaaarao',
+      customer_Address: 'muluugu',
+      billing_Num: '56555',
+      pickup_loc: 'waranagal',
+      pin_Code: 505136,
+      shipping_Date: '23/11/2023',
       user: newUser._id,
       crud: newCrud._id,
     });
-
+ 
     const newPaymentController = await PaymentController.create({
-      name: 'poja',
+      name: 'uyy',
       amount: 100,
       product_name: 'ewyyueywyu',
       description: 'gfdf',
@@ -281,95 +256,21 @@ app.get('/created', async (req, res) => {
       crud: newCrud._id,
       shipment: newShipment._id,
     });
-
-    const newUpload = await Upload.create({
-      file: 'nfre',
-      user: newUser._id,
-      crud: newCrud._id,
-      shipment: newShipment._id,
-      paymentController: newPaymentController._id,
-    });
-  
-    
-    // Update user with the new crud, shipment, upload, and paymentController
+ 
+    // Update user with the new crud, shipment, and paymentController
     newUser.cruds.push(newCrud._id);
     newUser.shipments.push(newShipment._id);
     newUser.paymentcontrollers.push(newPaymentController._id);
-    newUser.uploads.push(newUpload._id);
     await newUser.save();
-
-    res.send('User, Crud, Shipment, Upload, and PaymentController created successfully');
+ 
+    res.send('User, Crud, Shipment, and PaymentController created successfully');
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
   }
-
 });
-//   let newUser;
-//   try {
-//     const existingUser = await User.findOne({ companyname: 'snvsolutions' });
  
-//     if (existingUser) {
-//       existingUser.name = 'newName';
-//       await existingUser.save();
-//     } else {
-//       newUser = await User.create({
-//         name: 'deepika',
-//         email: 'deepu@gmail.com',
-//         mobile: 9899987679,
-//         password: 'deepu',
-//         companyname: 'snvsolutions',
-//       });
-//     }
- 
-   
-//       const newCrud = await Crud.create({
-//         order: 'lemon',
-//         date: '26/08/2028',
-//         payment: 7890,
-//         product: 'items',
-//         customer: 'revathi',
-//         phone: 8887678879,
-//         weight: '7.6',
-//         user: newUser._id,
-//       });
- 
-//       const newShipment = await Shipment.create({
-//         order_Id: 456765,
-//         customer_Name: 'swetha',
-//         customer_Address: 'nizampet',
-//         billing_Num: '876678',
-//         pickup_loc: 'ananatapur',
-//         pin_Code: 515401,
-//         shipping_Date: '18/09/2022',
-//         user: newUser._id,
-//         crud: newCrud._id,
-//       });
- 
-//       const newPaymentController = await PaymentController.create({
-//         name: 'nikhila',
-//         amount: 100,
-//         product_name: 'ironbox',
-//         description: 'dfgd',
-//         user: newUser._id,
-//         crud: newCrud._id,
-//         shipment: newShipment._id,
-//       });
- 
-//       newUser.cruds.push(newCrud._id);
-//       newUser.shipments.push(newShipment._id);
-//       newUser.paymentcontrollers.push(newPaymentController._id);
- 
-//       await newUser.save();
-//     }
- 
-//     res.send('User, Crud, Shipment, and PaymentController created successfully');
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message, message: 'Internal Server Error' });
-//   }
-// });
- 
+// Fetch all users with associated cruds, shipments, and paymentControllers
 app.get('/users', async (req, res) => {
   try {
     const users = await User.find()
@@ -387,13 +288,8 @@ app.get('/users', async (req, res) => {
         path: 'paymentcontrollers',
         model: 'PaymentController',
         select: 'name amount product_name description',
-      })
-      .populate({
-        path: 'uploads',  
-        model: 'Upload',  
-        select: 'file',
       });
-     
+ 
     res.json(users);
   } catch (error) {
     console.error(error);
@@ -401,7 +297,8 @@ app.get('/users', async (req, res) => {
   }
 });
  
- 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-})
+app.listen(8080, () => {
+  console.log(`Server is running on http://localhost:${8080}`);
+});
+
+
